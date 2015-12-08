de0482b85a063e0ad74fca2d05ad21b38c44a977
Brandon Baca <bbcharger13@gmail.com>
2015-12-08 10:37:05 -0600

    added a start screen and set it to transition to the GameScene

---
+++ Hand-and-Foot/StartScene.swift
@@ -0,0 +1,52 @@
+//
+//  StartScene.swift
+//  Hand-and-Foot
+//
+//  Created by Brandon Baca on 12/8/15.
+//  Copyright © 2015 Baca_Family. All rights reserved.
+//
+
+import Foundation
+
+
+import SpriteKit
+
+
+class StartScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let handandfoot = SKLabelNode(fontNamed:"Chalkduster")
+        handandfoot.text = "Hand and Foot";
+        handandfoot.fontSize = 60;
+        handandfoot.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        
+        let touchanywhere = SKLabelNode(fontNamed: "chalkduster")
+        touchanywhere.text = "Touch anywhere to beign";
+        touchanywhere.fontSize = 30;
+        touchanywhere.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMinY(self.frame)+150);
+        touchanywhere.fontColor = UIColor.redColor()
+        
+        
+        self.backgroundColor = UIColor.blackColor()
+        self.addChild(handandfoot)
+        self.addChild(touchanywhere)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        /* Called when a touch begins */
+        
+        let gamescene = GameScene(size: self.size)
+        let transition = SKTransition.flipHorizontalWithDuration(1.0)
+        
+        gamescene.scaleMode = SKSceneScaleMode.AspectFill
+        
+        self.scene?.view?.presentScene(gamescene, transition: transition)
+        
+        
+        
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
